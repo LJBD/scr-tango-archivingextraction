fe8d3d1fd24f335fde2622ac7be12a4e6863acf2
lukasz.dudek <lukasz.j.dudek@uj.edu.pl>
2015-11-30 10:14:26 +0100

    Added methods for plotting data.

--- database_connection.py
+++ database_connection.py
@@ -1,18 +1,20 @@
 import MySQLdb
 import re
 from collections import Counter
-from matplotlib import pyplot
+from matplotlib import pyplot, dates
 
 
 class DatabaseExtractor(object):
     def __init__(self):
         self.connection = MySQLdb.connect('localhost', 'root', 'solaris', 'tdb')
         self.cursor = self.connection.cursor()
+        self.get_all_attributes()
 
     def get_all_attributes(self):
         self.cursor.execute('SELECT full_name from adt;')
         self.devices_list = [i[0] for i in self.cursor.fetchall()]
         #print self.devices_list
+        return self.devices_list
 
     def get_selected_attributes(self, reg_exp_filter):
         matching_devices = filter(reg_exp_filter.match, self.devices_list)
@@ -39,10 +41,15 @@ class DatabaseExtractor(object):
         query = 'SELECT %s from %s where time > "%s" and time < "%s";' %\
                 (columns_str, table_name, start_date, end_date)
         self.cursor.execute(query)
-        return self.cursor.fetchall()
+        query_output = self.cursor.fetchall()
+        return_data = []
+        for i in xrange(len(columns)):
+            data_for_column = [t[i] for t in query_output]
+            return_data.append(data_for_column)
+        return return_data
 
     def calculate_time_diffs(self, id):
-        times = [i[0] for i in self.get_attrib_values_between_dates(id, '2015-06-01', '2020-01-01', ['time'])]
+        times = self.get_attrib_values_between_dates(id, '2015-06-01', '2020-01-01', ['time'])[0]
         print len(times)
         diffs = []
         for i in xrange(1, len(times)):
@@ -53,21 +60,58 @@ class DatabaseExtractor(object):
         pyplot.yscale('log')
         pyplot.show()
 
+    def plot_selected_attrib_data_in_range(self, re_filter, start, end):
+        attributes = self.get_selected_attributes(re_filter)
+        dev_dict = self.get_attrib_ids(attributes)
+        fig = pyplot.figure()
+        ax = fig.add_subplot(111)
+        for key in dev_dict.keys():
+            columns = ['time', 'read_value']
+            attr_values = self.get_attrib_values_between_dates(dev_dict[key], start, end, columns)
+            ax.plot(attr_values[0], attr_values[1], label=key)
+        pyplot.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+        ax.xaxis.set_major_formatter(dates.DateFormatter('%d.%m %H:%M:%S'))
+        pyplot.xticks(rotation='vertical')
+        pyplot.subplots_adjust(bottom=.3)
+        pyplot.show()
+
+    def plot_time_diffs_histogram(self, re_filter):
+        attributes = self.get_selected_attributes(re_filter)
+        dev_dict = self.get_attrib_ids(attributes)
+        for key in dev_dict.keys():
+            self.calculate_time_diffs(dev_dict[key])
+
 
 
 def main():
     dbEx = DatabaseExtractor()
-    dbEx.get_all_attributes()
-    re_filter = re.compile('PLC/R1-02-03-VAC/REAL/.*TCO.*_R')
-    thermocouples = dbEx.get_selected_attributes(re_filter)
-    dev_dict_thermos = dbEx.get_attrib_ids(thermocouples)
-    for key in dev_dict_thermos.keys():
-        print key, dev_dict_thermos[key],
-        # start = '2015-11-20 10:00:00'
-        # end = '2015-11-20 10:01:00'
-        # columns = ['time', 'read_value']
-        # dbEx.get_attrib_values_between_dates(dev_dict_thermos[key], start, end, columns)
-        dbEx.calculate_time_diffs(dev_dict_thermos[key])
+    start = '2015-11-18 09:00:00'
+    end = '2015-11-18 16:00:00'
+    re_filter = re.compile('R1-C134/MAG/R1.*-PS.*/Current')
+    dbEx.plot_selected_attrib_data_in_range(re_filter, start, end)
+    dbEx.plot_time_diffs_histogram(re_filter)
+
+    # thermocouples = dbEx.get_selected_attributes(re_filter)
+    # dev_dict_thermos = dbEx.get_attrib_ids(thermocouples)
+    # re_filter = re.compile('PLC/R1-02-03-VAC/REAL/F_R1_02.*TCO.*_R')
+    # SHOWING HISTOGRAM OF TIME DIFFS
+    # for key in dev_dict_thermos.keys():
+    #     print key, dev_dict_thermos[key],
+    #     # start = '2015-11-20 10:00:00'
+    #     # end = '2015-11-20 10:01:00'
+    #     # columns = ['time', 'read_value']
+    #     # dbEx.get_attrib_values_between_dates(dev_dict_thermos[key], start, end, columns)
+    #     dbEx.calculate_time_diffs(dev_dict_thermos[key])
+
+    # SHOWING SOME DATA FROM THERMOCOUPLES
+    # for key in dev_dict_thermos.keys():
+    #     start = '2015-11-20 16:00:00'
+    #     end = '2015-11-20 20:00:00'
+    #     columns = ['time', 'read_value']
+    #     attr_values = dbEx.get_attrib_values_between_dates(dev_dict_thermos[key], start, end, columns)
+    #     pyplot.plot(attr_values[0], attr_values[1], label=key.split('/')[3])
+    # pyplot.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    # pyplot.show()
 
 if __name__ == '__main__':
     main()
